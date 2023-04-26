9e5c869fe6eda9bfbecd21cc7c13d3f459028402
Adriano <adriano_alves_2008@hotmail.com>
2023-04-26 18:20:57 -0300

    Criando Header pt2

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,18 +3,27 @@ import Image from 'next/image'
 import logo from '../../../public/images/logo.svg'
 
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 function Header() {
+  const router = useRouter()
+  const { pathname } = router
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <a href="#">
+        <a href="/">
           <Image src={logo} alt="Logo" />
         </a>
         <nav>
-          <Link href="/">Home</Link>
-          <Link href="/posts">Conteúdos</Link>
-          <Link href="/sobre">Quem somos?</Link>
+          <Link href="/">
+            <p className={pathname === '/' ? 'active' : ''}>Home</p>
+          </Link>
+          <Link href="/posts">
+            <p className={pathname === '/posts' ? 'active' : ''}>Conteúdo</p>
+          </Link>
+          <Link href="/sobre">
+            <p className={pathname === '/sobre' ? 'active' : ''}>Quem somos?</p>
+          </Link>
         </nav>
 
         <a className={styles.readyButton} href="#" type="button">
