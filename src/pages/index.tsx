09cbedfa4c230daa068af58da21f6a55aeb71f86
Adriano <adriano_alves_2008@hotmail.com>
2023-04-26 18:35:22 -0300

    Criando pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
+import styles from '../styles/home.module.scss'
+import Head from 'next/head'
+
 export default function Home() {
   return (
-    <div>
-      <h1>Teste</h1>
-    </div>
+    <>
+      <Head>
+        <title>Apaixonado por tecnologia</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.containerHeader}>
+          <section className={styles.ctaText}>
+            <h1>Levando você ao próximo nível</h1>
+            <span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam iure,
+              illo dolorum eum ipsa facere iusto.
+            </span>
+            <a>
+              <button>COMEÇAR AGORA!</button>
+            </a>
+          </section>
+
+          <img src="/images/banner-conteudos.png" alt="Conteúdos" />
+        </div>
+      </main>
+    </>
   )
 }
