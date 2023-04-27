9c0d0d50fec3ea53ade4898c670bd4f1858c47cd
Adriano <adriano_alves_2008@hotmail.com>
2023-04-27 13:16:07 -0300

    Atualizando pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,12 +34,9 @@ export default function Home({ content }: ContentProps) {
       <main className={styles.container}>
         <div className={styles.containerHeader}>
           <section className={styles.ctaText}>
-            <h1>Levando você ao próximo nível</h1>
-            <span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam iure,
-              illo dolorum eum ipsa facere iusto.
-            </span>
-            <a>
+            <h1>{content.title}</h1>
+            <span>{content.titleContent}</span>
+            <a href={content.linkAction}>
               <button>COMEÇAR AGORA!</button>
             </a>
           </section>
@@ -51,26 +48,20 @@ export default function Home({ content }: ContentProps) {
 
         <div className={styles.sectionContent}>
           <section>
-            <h2>Aprenda a criar aplicativos para Android e IOS</h2>
-            <span>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime
-              harum veniam architecto itaque officiis.
-            </span>
+            <h2>{content.mobileTitle}</h2>
+            <span>{content.mobileContent}</span>
           </section>
-          <img src="/images/financasApp.png" alt="Conteúdos mobile" />
+          <img src={content.mobileBanner} alt="Conteúdos mobile" />
         </div>
 
         <hr className={styles.divisor} />
 
         <div className={styles.sectionContent}>
-          <img src="/images/webDev.png" alt="Conteúdos mobile" />
+          <img src={content.webBanner} alt="Conteúdos mobile" />
 
           <section>
-            <h2>Aprenda a criar sistemas web</h2>
-            <span>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime
-              harum veniam architecto itaque officiis.
-            </span>
+            <h2>{content.webTitle}</h2>
+            <span>{content.webContent}</span>
           </section>
         </div>
 
@@ -83,7 +74,7 @@ export default function Home({ content }: ContentProps) {
           <span>
             E você vai perder a chance de evoluir de uma vez por todas?
           </span>
-          <a href="">
+          <a href={content.linkAction}>
             <button>COMEÇAR AGORA!</button>
           </a>
         </div>
